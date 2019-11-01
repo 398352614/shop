9da8898c3984633a49dac0e02a5a93bb9d437eb5
whoyummy <398352614@qq.com>
2019-11-01 17:56:46 +0800

    收货地址列表

--- routes/web.php
+++ routes/web.php
@@ -14,8 +14,14 @@
 
 
 
-Route::get('/', 'PagesController@root')->name('root');
+Route::get('/', 'PagesController@root')->name('root')->middleware('verified');
+Auth::routes(['verify' => true]);
+Route::group(['middleware'=>['auth','verified']],function (){
+    Route::get('user_addresses','UserAddressesController@index')
+        ->name('user_addresses.index');
 
-Auth::routes();
+});
+
+
 
 
