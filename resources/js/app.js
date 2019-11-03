e79e835b68db7c807cbeee48d29d88b56dd9b720
whoyummy <398352614@qq.com>
2019-11-03 12:03:05 +0800

    新增及修改收货地地址

--- resources/js/app.js
+++ resources/js/app.js
@@ -8,7 +8,7 @@
 require('./bootstrap');
 
 window.Vue = require('vue');
-require('./components/SelectDistrict');
+require('./components/UserAddressesCreateAndEdit');
 /**
  * The following block of code may be used to automatically register your
  * Vue components. It will recursively scan this directory for the Vue
@@ -27,6 +27,7 @@ Vue.component('example-component', require('./components/ExampleComponent.vue').
  * the page. Then, you may begin adding components to this application
  * or customize the JavaScript scaffolding to fit your unique needs.
  */
+require('./components/SelectDistrict');
 
 const app = new Vue({
     el: '#app',
