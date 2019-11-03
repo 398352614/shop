dd7d4cd3a49b9d9a8f38096325c3ff0b94dd62c5
whoyummy <398352614@qq.com>
2019-11-03 14:52:24 +0800

    修改删除

--- app/Http/Controllers/UserAddressesController.php
+++ app/Http/Controllers/UserAddressesController.php
@@ -20,6 +20,12 @@ class UserAddressesController extends Controller
     public function create(){
         return view('user_addresses.create_and_edit',['address'=>new UserAddress()]);
     }
+
+    public function edit(UserAddress $userAddress){
+        $this->authorize('own', $user_address);
+        return view('user_addresses.create_and_edit',['address'=>$userAddress]);
+    }
+
     public function store(UserAddressRequest $request)
     {
         $request->user()->addresses()->create($request->only([
@@ -31,8 +37,31 @@ class UserAddressesController extends Controller
             'contact_name',
             'contact_phone',
         ])); return redirect()->route('user_addresses.index');
+    }
+
+
+    public function update(UserAddress $user_address, UserAddressRequest $request){
+        $this->authorize('own', $user_address);
+        $user_address->update($request->only([
+            'province',
+            'city',
+            'district',
+            'address',
+            'zip',
+            'contact_name',
+            'contact_phone',
+        ]));
+        return redirect()->route('user_addresses.index');
+    }
+
+    public function destroy(UserAddress $user_address)
+    {
+        $this->authorize('own', $user_address);
+        $user_address->delete();
+
+        return [];
+    }
 }
-}   //
 
 
 
---
+++ app/Policies/UserAddressPolicy.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Policies;
+
+use App\Models\User;
+use App\Models\UserAddress;
+use Illuminate\Auth\Access\HandlesAuthorization;
+
+class UserAddressPolicy
+{
+    use HandlesAuthorization;
+    public function own(User $user, UserAddress $address)
+    {
+        return $address->user_id == $user->id;
+    }
+    /**
+     * Create a new policy instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+}
