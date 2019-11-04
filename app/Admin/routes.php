e672541453fc8f29d0335447fecbd0d55f781d75
whoyummy <398352614@qq.com>
2019-11-04 14:01:07 +0800

    管理后台

--- app/Admin/routes.php
+++ app/Admin/routes.php
@@ -12,4 +12,5 @@ Route::group([
 ], function (Router $router) {
     $router->get('/', 'HomeController@index');
     $router->get('users', 'UsersController@index');
+    $router->get('products', 'ProductsController@index');
 });
--- database/migrations/2019_11_04_045525_create_products_table.php
+++ database/migrations/2019_11_04_045525_create_products_table.php
@@ -15,14 +15,14 @@ class CreateProductsTable extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->bigIncrements('id');
-            $table->string('tittle');
+            $table->string('title');
             $table->text('description');
             $table->string('image');
             $table->boolean('on_sale')->default(true);
             $table->float('rating')->default(5);
             $table->unsignedInteger('sold_count')->default(0);
             $table->unsignedInteger('review_count')->default(0);
-            $table->decimal('price',10,2);
+            $table->decimal('price', 10, 2);
             $table->timestamps();
         });
     }
