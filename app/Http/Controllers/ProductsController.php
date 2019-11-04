37b5c8bcea0447e28bcde2062d5c09b1207f43a1
whoyummy <398352614@qq.com>
2019-11-04 19:02:11 +0800

    11.4

--- app/Http/Controllers/ProductsController.php
+++ app/Http/Controllers/ProductsController.php
@@ -8,6 +8,27 @@ class ProductsController extends Controller
 {
     public function index(Request $request)
     {
+        // 创建一个查询构造器
+        $builder = Product::query()->where('on_sale', true);
+        // 判断是否有提交 search 参数，如果有就赋值给 $search 变量
+        // search 参数用来模糊搜索商品
+        if ($search = $request->input('search', '')) {
+            $like = '%'.$search.'%';
+            // 模糊搜索商品标题、商品详情、SKU 标题、SKU描述
+            $builder->where(function($query)use($like){
+                $query->where('title','like',$like)
+                    ->orwhere('description','like',$like)
+                    ->orwherehas('skus',function($query)use($like){
+                    $query->where('title','like',$like)
+                        ->orwhere('description','like',$like);
+                });
+            });
+}
+
+
+
+
+
         $products = Product::query()->where('on_sale', true)->paginate(16);
 
         return view('products.index', ['products' => $products]);
